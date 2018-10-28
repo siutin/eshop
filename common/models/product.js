76c60e38d4a95dfbcb017cd40dc07686d4c2bbf3
osiutino <osiutino@gmail.com>
2018-10-28 16:51:54 +0800

    add a Product Search API

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function(Product) {
+    Product.search = function(name, cb) {        
+        Product.find({where: {name: { like: `%${name}%`}}, limit: 10}, cb)
+    }
 
+    Product.remoteMethod('search', {
+        accepts: {arg: 'name', type: 'string'},
+        returns: {arg: 'product', type: 'Array'},
+        http: {verb: 'get' }
+  });
 };
