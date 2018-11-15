4badb0551c960b107a7ca13bd4c69d71efb88eb9
martin <osiutino@gmail.com>
2018-11-15 16:55:05 +0800

    sample - create product, staff and roles. map roles to staffs

---
+++ server/boot/sample.js
@@ -0,0 +1,109 @@
+import Safe from '../safe'
+
+module.exports = async function (app) {
+
+  const Shop = app.models.Shop
+  const ShopStaff = app.models.ShopStaff
+
+  const Role = app.models.Role
+  const RoleMapping = app.models.RoleMapping
+
+  let err, isCreated;
+
+  // CREATE SHOPS ------------------------------------------------------------
+  let shop = await Shop.findOne({ where: { name: 'Apple' } })
+  console.log(shop)
+
+  // CREATE STAFFS ------------------------------------------------------------
+  let staff;
+  [err, [staff, isCreated]] = await Safe(ShopStaff.findOrCreate({
+    shopId: shop.id,
+    username: 'appleStaff',
+    email: 'appleStaff@example.com',
+  }, { password: '12345678' }));
+
+  [err, staff] = await Safe(staff.save())
+
+  if (err)
+    throw `ERROR: ${err}`
+
+  console.log(`isCreated: ${isCreated}`)
+  console.log(staff)
+
+  // CREATE ROLES ------------------------------------------------------------
+  let role;
+  [err, [role, isCreated]] = await Safe(Role.findOrCreate({
+    name: 'staff'
+  }))
+
+  if (err)
+       throw `ERROR: ${err}`
+
+  console.log(`isCreated: ${isCreated}`)
+  console.log(role)
+
+  // CREATE Principals ------------------------------------------------------------
+
+  let data = {
+    principalType: RoleMapping.USER,
+    principalId: staff.id
+  }
+
+  let principal;
+  [err, principal] = await Safe(role.principals.findOne(data))
+
+  if (err)
+    throw `ERROR: ${err}`
+
+  if (!principal) {
+    console.log("principal not exists")
+    [err, principal] = await Safe(
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: staff.id
+      })
+    )
+    if (err)
+      throw `ERROR: ${err}`
+  }
+
+  console.log(principal)
+
+  // [err, [principal, isCreated]] = await Safe(role.principals.create({
+  //   principalType: RoleMapping.USER,
+  //   principalId: 1
+  // }))
+
+  // if (err) {
+  //   console.log(`ERROR: ${err}`)
+  //   return
+  // }
+
+  // console.log(`isCreated: ${isCreated}`)
+  // console.log(principal)
+
+  // Shop.findOne({})
+  // User.findOne({
+  //     where: {
+  //       username: 'martin'
+  //     }
+  //   }, function (err, user) {
+  //     Role.findOne({
+  //         where: {
+  //           name: 'admin'
+  //         },
+  //         function (err, role) {
+  //           if (err) throw err
+
+  //           //make bob an admin
+  //           role.principals.create({
+  //             principalType: RoleMapping.USER,
+  //             principalId: user.id
+  //           }, function (err, principal) {
+  //             throw err
+  //           })
+  //         }
+  //       })
+  //     })
+
+}
