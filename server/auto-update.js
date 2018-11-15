1fddbdc3dc5b40dbf31ae9a16d5d59070c52957c
martin <osiutino@gmail.com>
2018-11-15 15:37:41 +0800

    migration auto update - dynamic detect new models

--- server/auto-update.js
+++ server/auto-update.js
@@ -6,7 +6,11 @@ const dataSource = dataSources['eshopDevelopment']
 if (!dataSource) {
   throw new Error('dataSource not found')
 }
-dataSource.autoupdate(['Product', 'ACL'], function (error) {
+
+const modelNames = Object.keys(server.models)
+
+console.log(`modelNames: ${modelNames.join(", ")}`)
+dataSource.autoupdate(modelNames, function (error) {
     if (error) {
       console.error(`Aup Update Failed, MSG: ${error}`)
     } else {
