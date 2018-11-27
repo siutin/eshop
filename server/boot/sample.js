f243beee22baf6162053b8b86c3798910bb90c59
martin <osiutino@gmail.com>
2018-11-27 18:37:33 +0800

    sample - extract saveOrThrow

--- server/boot/sample.js
+++ server/boot/sample.js
@@ -1,7 +1,20 @@
 import Safe from '../safe'
 
+async function saveOrThrow(fnCall) {
+    let err, isCreated;
+    let resource;
+    [err, [resource, isCreated]] = await Safe(fnCall);
+    [err, resource] = await Safe(resource.save())
+    if (err)
+      throw `ERROR: ${err}`
+
+    console.log(`isCreated: ${isCreated}`)
+    return resource
+}
+
 module.exports = async function (app) {
 
+  const Product = app.models.Shop
   const Shop = app.models.Shop
   const ShopStaff = app.models.ShopStaff
   const ShopMember = app.models.ShopMember
@@ -9,80 +22,82 @@ module.exports = async function (app) {
   const Role = app.models.Role
   const RoleMapping = app.models.RoleMapping
 
-  let err, isCreated;
+  // CREATE SHOPS & Products ------------------------------------------------------------
 
-  // CREATE SHOPS ------------------------------------------------------------
-  let shop = await Shop.findOne({ where: { name: 'Apple' } })
-  console.log(shop)
+  let shop1 = await saveOrThrow(Shop.findOrCreate({name: 'Apple'}))
+  console.log('shop1:', shop1)
+
+  let shop2 = await saveOrThrow(Shop.findOrCreate({ name: 'Samsung' }))
+  console.log('shop2:', shop2)
 
   // CREATE STAFFS ------------------------------------------------------------
-  let staff;
-  [err, [staff, isCreated]] = await Safe(ShopStaff.findOrCreate({
-    shopId: shop.id,
-    username: 'appleStaff',
-    email: 'appleStaff@example.com',
-  }, { password: '12345678' }));
-
-  [err, staff] = await Safe(staff.save())
-
-  if (err)
-    throw `ERROR: ${err}`
-
-  console.log(`isCreated: ${isCreated}`)
-  console.log(staff)
+  let staff1 = await saveOrThrow(
+    ShopStaff.findOrCreate({
+          shopId: shop1.id,
+          username: 'appleStaff',
+          email: 'appleStaff@example.com',
+        }, {
+          password: '12345678'
+        }
+    )
+  );
+  console.log('staff1:', staff1)
 
   // CREATE MEMBERS ------------------------------------------------------------
-  let member;
-  [member, isCreated] = await ShopMember.findOrCreate({
-    where: {
-      shopId: shop.id,
-      username: 'member01',
-      email: 'member01@example.com',
-    }
-  }, {
-    password: '12345678'
-  })
 
-  console.log(isCreated)
-  console.log(member)
+  let member1 = await saveOrThrow(
+    ShopMember.findOrCreate({
+          where: {
+            shopId: shop1.id,
+            username: 'member01',
+            email: 'member01@example.com',
+          }
+        }, {
+          password: '12345678'
+        }
+    )
+  );
+  console.log('member1:', member1)
+
   // CREATE ROLES ------------------------------------------------------------
-  let role;
-  [err, [role, isCreated]] = await Safe(Role.findOrCreate({
-    name: 'staff'
-  }))
 
-  if (err)
-       throw `ERROR: ${err}`
-
-  console.log(`isCreated: ${isCreated}`)
-  console.log(role)
+  let role1 = await saveOrThrow(
+    Role.findOrCreate({
+      name: 'staff'
+    })
+  );
+  console.log('role1:', role1)
 
   // CREATE Principals ------------------------------------------------------------
 
-  let data = {
-    principalType: RoleMapping.USER,
-    principalId: staff.id
-  }
+  // let data = {
+  //   principalType: RoleMapping.USER,
+  //   principalId: staff.id
+  // }
 
-  let principal;
-  [err, principal] = await Safe(role.principals.findOne(data))
+  // let principal;
+  // [err, principal] = await Safe(role.principals.findOne(data))
 
-  if (err)
-    throw `ERROR: ${err}`
+  // if (err)
+  //   throw `ERROR: ${err}`
 
-  if (!principal) {
-    console.log("principal not exists")
-    [err, principal] = await Safe(
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: staff.id
-      })
-    )
-    if (err)
-      throw `ERROR: ${err}`
-  }
+  // if (!principal) {
+  //   console.log("principal not exists")
+  //   [err, principal] = await Safe(
+  //     role.principals.create({
+  //       principalType: RoleMapping.USER,
+  //       principalId: staff.id
+  //     })
+  //   )
+  //   if (err)
+  //     throw `ERROR: ${err}`
+  // }
 
-  console.log(principal)
+  // console.log(principal)
+
+
+/// ---
+
 
   // [err, [principal, isCreated]] = await Safe(role.principals.create({
   //   principalType: RoleMapping.USER,
